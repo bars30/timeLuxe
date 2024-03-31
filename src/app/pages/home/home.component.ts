c621bdc39c464c40f2a705d8fdc920546c2644b6
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-03-31 05:28:38 +0400

    done some changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -92,7 +92,7 @@ export class HomeComponent {
   }
 
   initialAnimations(): void {
-    gsap.set(this.backImg.nativeElement, { scale: 0.8 });
+    gsap.set(this.backImg.nativeElement, { scale: 1 });
     gsap.set(this.brandName.nativeElement, { translate: '-50px;' });
     gsap.set(this.brandDesc.nativeElement, { translate: '50px;' });
 
@@ -100,8 +100,8 @@ export class HomeComponent {
 
   initScrollAnimations(): void {
     gsap.to(this.backImg.nativeElement, {
-      scale: 1.3,
-      duration: 1, // Specify the duration here (in seconds)
+
+      duration: 0, // Specify the duration here (in seconds)
       scrollTrigger: {
         trigger: this.backImg.nativeElement,
         scrub: true
@@ -168,42 +168,97 @@ export class HomeComponent {
 
     
   }
-  watch(){
+  // watch(){
+  //   this.canvas.nativeElement.width = window.innerWidth;
+  //   this.canvas.nativeElement.height = window.innerHeight;
+
+  //   const context = this.canvas.nativeElement.getContext("2d");
+  //   const frameCount = 73;
+  //   const currentFrame = (index: any) => `/assets/img/watch/${(index + 1).toString()}.jpg`;
+  //   const fimg = new Image();
+  //   fimg.src = '/assets/img/watch/242.jpg'
+  //   const images = [fimg];
+  //   let ball = {frame: 0}
+  //   // console.log(images);
+    
+  //   for(let i = 15; i < frameCount; i++){
+  //     const img = new Image();
+  //     img.src = currentFrame(i);
+  //     images.push(img)
+  //     // console.log(images);
+      
+  //    }
+  //    gsap.to(ball, {
+  //     frame: frameCount - 1,
+  //     snap: "frame",
+  //     ease: "none",
+  //     scrollTrigger: {
+  //      scrub: true,
+  //      pin: "canvas",
+  //      end: "500%",
+  //      // markers: true
+  //     },
+  //     onUpdate: render,
+  //    })
+
+  //    images[0].onload = render;
+  //   //  console.log(images);
+  //    gsap.fromTo(
+  //     ".ball-text",
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       scrollTrigger: {
+  //         scrub: 1,
+     
+  //         start: "50%",
+  //         end: "60%",
+  //       },
+  //       onComplete: () => {
+  //         gsap.to(".ball-text", { opacity: 0 });
+  //       },
+  //     }
+  //    );
+
+  //    function render() {
+  //     if(!context){return}
+  //     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  //     context.drawImage(images[ball.frame], 0, 0, window.innerWidth, window.innerHeight);
+  //    }
+  //   }
+  watch() {
     this.canvas.nativeElement.width = window.innerWidth;
     this.canvas.nativeElement.height = window.innerHeight;
-
+  
     const context = this.canvas.nativeElement.getContext("2d");
-    const frameCount = 314;
-    const currentFrame = (index: any) => `/assets/img/watch/${(index + 1).toString()}.jpg`;
-    const fimg = new Image();
-    fimg.src = '/assets/img/watch/2.jpg'
-    const images = [fimg];
-    let ball = {frame: 0}
-    // console.log(images);
-    
-    for(let i = 0; i < frameCount; i++){
+    const frameCount = 79;
+    const images: any = [];
+  
+    for (let i = 0; i < frameCount; i++) {
       const img = new Image();
-      img.src = currentFrame(i);
-      images.push(img)
-      // console.log(images);
-      
-     }
-     gsap.to(ball, {
+      img.src = `/assets/img/watch/${i + 1}.png`;
+      images.push(img);
+    }
+  
+    let ball = { frame: 0 };
+  
+    gsap.to(ball, {
       frame: frameCount - 1,
       snap: "frame",
       ease: "none",
       scrollTrigger: {
-       scrub: true,
-       pin: "canvas",
-       end: "500%",
-       // markers: true
+        scrub: true,
+        pin: "canvas",
+        end: "500%",
       },
       onUpdate: render,
-     })
-
-     images[0].onload = render;
-    //  console.log(images);
-     gsap.fromTo(
+    });
+  
+    images[0].onload = render;
+  
+    gsap.fromTo(
       ".ball-text",
       {
         opacity: 0,
@@ -212,7 +267,6 @@ export class HomeComponent {
         opacity: 1,
         scrollTrigger: {
           scrub: 1,
-     
           start: "50%",
           end: "60%",
         },
@@ -220,12 +274,13 @@ export class HomeComponent {
           gsap.to(".ball-text", { opacity: 0 });
         },
       }
-     );
-
-     function render() {
-      if(!context){return}
+    );
+  
+    function render() {
+      if (!context) return;
       context.clearRect(0, 0, window.innerWidth, window.innerHeight);
       context.drawImage(images[ball.frame], 0, 0, window.innerWidth, window.innerHeight);
-     }
     }
+  }
+  
 }
